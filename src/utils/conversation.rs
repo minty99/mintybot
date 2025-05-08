f383f937a20b54921a891fc2f77e8cba4282a885
Muhwan Kim <mhkim4886@gmail.com>
2025-05-08 23:00:22 +0900

    Add `Display` impl for `ChatMessage`

--- src/utils/conversation.rs
+++ src/utils/conversation.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use serenity::model::id::ChannelId;
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -15,6 +16,24 @@ pub struct ChatMessage {
     pub content: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+}
+
+impl Display for ChatMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let role_str = format!("<{}>", self.role);
+        let name_str = self
+            .name
+            .as_ref()
+            .map(|name| format!("({name})"))
+            .unwrap_or_default();
+        let content_str = self.content.to_string();
+
+        write!(
+            f,
+            "{}",
+            itertools::join([role_str, name_str, content_str], " ")
+        )
+    }
 }
 
 impl ChatMessage {
